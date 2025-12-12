8903243877011a45071fc9c059821649892939a9
futuresmartinvest-pixel <futuresmartinvest@gmail.com>
2025-12-12 03:15:01 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-// Your Firebase Config
+// Your Firebase Config (corrected)
 const firebaseConfig = {
   apiKey: "AIzaSyA1HZLZRY9UADNnDVBnoBIlG3b-bCkojxs",
   authDomain: "primeinvestproject2.firebaseapp.com",
   projectId: "primeinvestproject2",
-  storageBucket: "primeinvestproject2.appspot.com",   // ✅ FIXED
+  storageBucket: "primeinvestproject2.appspot.com",   // ✔ FIXED
   messagingSenderId: "798977477628",
   appId: "1:798977477628:web:c97d1fba72ad7865864079"
 };
